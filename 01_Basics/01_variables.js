0d0d1d55b119d61aca8e1a982255f036474168e7
Pranavigoud <sathuripranavigoud12@gmail.com>
2025-07-06 16:30:57 +0530

    code add chesina

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1234567
+let accountName = "vghj"
+var accountPass = "121212"
+accountCity = "jaipur"
+let accountState
+// accountId=2
+accountName="sadfg"
+accountPass="212121"
+accountCity="wedfg"
+console.log(accountId);
+console.table([accountId, accountName, accountPass, accountCity, accountState])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all js code as newer version
+// alert(3+3) we are using node.js not browser
+console.log(3+3); console.log("hitesh"); //code readability should be high 
+
+let name ="hitesh"
+let age = 18
+let isLoggedIn = false
+let state=null
+let code;
+//primitive datatypes
+//number=> 2 to power 53
+//bigint 
+//string => "" or ''
+//boolean => true/false
+//null => it is datatype and also standalone value and it is object when we run the typeof code
+//undefined => not defined
+//symbol => unique
+
+//object
+
+console.log(typeof state); //object
+console.log(typeof code); //undefined
