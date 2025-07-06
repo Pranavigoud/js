47b9d496b42df7a854d4e64055e79eeffd46b36a
Pranavigoud <sathuripranavigoud12@gmail.com>
2025-07-06 22:22:24 +0530

    updated conversion operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -20,4 +20,44 @@ let poolenIsLoggedIn = Boolean(isLoggedIn)
 // "hi"=>true
 let someNumber=33
 let strNumber = String(someNumber)
-console.log(typeof strNumber);
+// console.log(typeof strNumber);
+//**********operations************ */
+
+let value =3
+let negValue= -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="hello"
+let str2=" pranu"
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + 2);
+// console.log("1"+"2");
+// console.log((3+4)*5%3);
+
+console.log(true);
+console.log(+"");
+console.log(+true);
+// console.log(true+); // not accepted
+
+let num1, num2, num3
+num1=num2=num3=2+2
+console.log(num2);
+
+let gameCounter =100
+// gameCounter++
+++gameCounter
+// learn from mdn prefix and postfix
+console.log(gameCounter);
