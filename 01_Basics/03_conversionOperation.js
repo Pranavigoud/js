5a663780737ae450f5cd35c8859d4419ebc40299
Pranavigoud <sathuripranavigoud12@gmail.com>
2025-07-06 21:43:29 +0530

    conversions

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = "hitesh"
+let score = true
+
+// console.log(typeof score);
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// 33=> 33
+// "33abc"=>nan
+// true/false = 1/0
+let isLoggedIn = 1
+// let isLoggedIn=""
+let poolenIsLoggedIn = Boolean(isLoggedIn)
+// console.log(poolenIsLoggedIn);
+// 1=>true 0=>false
+// ""=> false 
+// "hi"=>true
+let someNumber=33
+let strNumber = String(someNumber)
+console.log(typeof strNumber);
